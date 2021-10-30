f5efb3ee6778e2d2577b1acea267832dc315a347
turki <nikolai.turkin.main@gmail.com>
2021-10-31 00:28:10 +0300

    careful added

--- js/script.js
+++ js/script.js
@@ -31,12 +31,13 @@ let q3 = {
     ]
 }
 
-let QQ = []
+let QQ = [];
 QQ.push(q1);
 QQ.push(q2);
 QQ.push(q3)
 let qq = 0;
 let btnHTML = '<input type="button" value="Ответить" class="btn" onclick="next()"> &nbsp <input type="button" value="Сбросить" class="btn" onclick="reset()">';
+let carefulHTML = ''
 function genQuestion() {
     let ln = QQ.length;
     if (ln == qq) {
@@ -55,19 +56,19 @@ function genQuestion() {
             form = form + genRadio(question.answer[i], i);
         }
     }
-    form = form + btnHTML;
+    form = form + carefulHTML + btnHTML;
     document.getElementById('qName').innerHTML = '<label class="qlabel">' + question.quest + '</label>';
     document.getElementById('question').innerHTML = form;
     qq++;
 }
 
 function genCheckbox(text, value) {
-    let check = '<label class="check option"> <input class="check_input" type="checkbox" value="' + value + '"> <span class="check_box"></span> ' + text + ' </label>';
+    let check = '<label class="check option"> <input class="check_input" type="checkbox" id="' + value + '"> <span class="check_box"></span> ' + text + ' </label>';
     return check;
 }
 
 function genRadio(text, value) {
-    let radio = '<label class="rad option"> <input type="radio" name="same" class="rad_input" value="' + value + '"> <span class="rad_box"></span>' + text + '</label>';
+    let radio = '<label class="rad option"> <input type="radio" name="same" class="rad_input" id="' + value + '"> <span class="rad_box"></span>' + text + '</label>';
     return radio;
 }
 
@@ -77,10 +78,31 @@ function reset() {
 }
 
 function next() {
-    genQuestion();
+    if (check()) {
+        genQuestion();
+    }
+    else {
+        qq--;
+        carefulHTML = '<div style=" margin-bottom: 10px"><label style="color: red; font-size: 16px;"> Выберите вариант ответа </label> <br> </div>';
+        genQuestion();
+        carefulHTML = '';
+    }
 }
 
 function last() {
     document.getElementById('qName').innerHTML = '';
     document.getElementById('question').innerHTML = '<h4>Вопросы закончились</h4>';
+}
+
+function check() {
+    let vls = [];
+    question = QQ[qq - 1];
+    for (let i = 0; i < question.length; i++) {
+        vls.push(document.getElementById('' + i).checked);
+    }
+    console.log(vls);
+    if (vls.indexOf(true) != -1) {
+        return true;
+    }
+    return false;
 }
