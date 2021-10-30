5aaac297448852aa02d0fb3b45f9561d40833bd0
turki <nikolai.turkin.main@gmail.com>
2021-10-30 23:33:16 +0300

    q3 added

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ let q1 = {
 let q2 = {
     type: 'checkbox',
     length: 4,
-    quest: 'Вопрос №1',
+    quest: 'Вопрос №2',
     answer: [
         'Вариант ответа №1',
         'Вариант ответа №2',
@@ -21,9 +21,20 @@ let q2 = {
     ]
 }
 
+let q3 = {
+    type: 'checkbox',
+    length: 2,
+    quest: 'Вопрос №3',
+    answer: [
+        'Да',
+        'Нет'
+    ]
+}
+
 let QQ = []
 QQ.push(q1);
 QQ.push(q2);
+QQ.push(q3)
 let qq = 0;
 let btnHTML = '<input type="button" value="Ответить" class="btn" onclick="next()"> &nbsp <input type="button" value="Сбросить" class="btn" onclick="reset()">';
 function genQuestion() {
