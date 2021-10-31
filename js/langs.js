4d2118e043ef9994e6272c3b8a078985ca27636c
turki <nikolai.turkin.main@gmail.com>
2021-10-31 13:51:03 +0300

    langs added

---
+++ js/langs.js
@@ -0,0 +1,147 @@
+let python = {
+    name: 'python',
+    universality: 'high',
+    complexity: 'low',
+    threshold: 'low',
+    relevance: 'high',
+    power: 'medium'
+}
+
+let cpp = {
+    name: 'cpp',
+    universality: 'high',
+    complexity: 'high',
+    threshold: 'medium',
+    relevance: 'high',
+    power: 'high'
+}
+
+let csharp = {
+    name: 'csharp',
+    universality: 'low',
+    complexity: 'high',
+    threshold: 'medium',
+    relevance: 'high',
+    power: 'high'
+}
+
+let c = {
+    name: 'c',
+    universality: 'medium',
+    complexity: 'high',
+    threshold: 'high',
+    relevance: 'high',
+    power: 'high'
+}
+
+let java = {
+    name: 'java',
+    universality: 'high',
+    complexity: 'medium',
+    threshold: 'low',
+    relevance: 'high',
+    power: 'medium'
+}
+
+let javascript = {
+    name: 'javascript',
+    universality: 'high',
+    complexity: 'medium',
+    threshold: 'low',
+    relevance: 'high',
+    power: 'medium'
+}
+
+let kotlin = {
+    name: 'kotlin',
+    universality: 'low',
+    complexity: 'medium',
+    threshold: 'medium',
+    relevance: 'medium',
+    power: 'medium'
+}
+
+let ruby = {
+    name: 'ruby',
+    universality: 'low',
+    complexity: 'high',
+    threshold: 'medium',
+    relevance: 'low',
+    power: 'high'
+}
+
+let php = {
+    name: 'php',
+    universality: 'low',
+    complexity: 'low',
+    threshold: 'low',
+    relevance: 'medium',
+    power: 'medium'
+}
+
+let swift = {
+    name: 'swift',
+    universality: 'medium',
+    complexity: 'medium',
+    threshold: 'medium',
+    relevance: 'high',
+    power: 'medium'
+}
+
+let html = {
+    name: 'html',
+    universality: 'low',
+    complexity: 'low',
+    threshold: 'low',
+    relevance: 'high',
+    power: 'high'
+}
+
+let visualbasic = {
+    name: 'visualbasic',
+    universality: 'low',
+    complexity: 'low',
+    threshold: 'low',
+    relevance: 'medium',
+    power: 'low'
+}
+
+let sql = {
+    name: 'sql',
+    universality: 'low',
+    complexity: 'medium',
+    threshold: 'medium',
+    relevance: 'high',
+    power: 'high'
+}
+
+let obj_c = {
+    name: 'obj_c',
+    universality: 'low',
+    complexity: 'high',
+    threshold: 'medium',
+    relevance: 'low',
+    power: 'medium'
+}
+
+let css = {
+    name: 'css',
+    universality: 'low',
+    complexity: 'low',
+    threshold: 'low',
+    relevance: 'high',
+    power: 'high'
+}
+
+let GAMES = [];
+let SITES = [];
+let IOS = [];
+let ANDROID = [];
+let MACOS = [];
+let WINDOWS = [];
+let LINUX = [];
+let MICRO = [];
+let BASE = [];
+let IIMO = [];
+
+GAMES.push()
